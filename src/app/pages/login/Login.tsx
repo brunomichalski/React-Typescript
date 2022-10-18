c9bb3709251ae4eebc7e2b2b118b1c2808adbe2a
Bruno Michalski <bruno_michalski@live.com>
2022-10-18 11:20:17 -0300

    Iniciando tela de login

---
+++ src/app/pages/login/Login.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleEntrar = () => {
+        console.log(email);
+        console.log(password);
+    };
+
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="">
+                    <span>Email</span>
+                    <input
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </label>
+
+                <label htmlFor="">
+                    <span>Senha</span>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+
+                <button type="button" onClick={handleEntrar}>
+                    Entrar
+                </button>
+            </form>
+        </div>
+    );
+};
